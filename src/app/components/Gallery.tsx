3903630823db2599252e08c2d6a8e93ac9df90e1
Mihir Pesswani <pesswani.mihir@gmail.com>
2023-09-20 21:30:12 +0530

    fetched remote images

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import fetchImages from "@/lib/fetchImages";
 import type { ImageResults } from "@/models/Images";
 import React from 'react'
+import ImgContainer from "./ImgContainer";
 
 export default async function Gallery() {
 
@@ -12,7 +13,7 @@ export default async function Gallery() {
     return (
         <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
             {images.photos.map(photo => (
-                <div key={photo.id} className="h-64 bg-gray-200 rounded-xl"></div>
+                <ImgContainer photo={photo} />
             ))}
         </section>
     )
---
+++ src/app/components/ImgContainer.tsx
@@ -0,0 +1,18 @@
+import type { Photo } from "@/models/Images"
+import Image from "next/image"
+
+type Props = {
+    photo: Photo
+};
+
+export default function ImgContainer({ photo }: Props) {
+    return (
+        <div key={photo.id} className="h-6 bg-gray-200 rounded-xl">
+            <Image
+                src={photo.src.large}
+                alt={photo.alt}
+                width={250}
+                height={250} />
+        </div>
+    )
+}
